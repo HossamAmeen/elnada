74446bd6a4c0cf4afaaf56fbe3fc9a3f1bb1b9c9
hossam <hossamameen948@gmail.com>
2021-04-15 17:21:34 +0200

    edit phone and whats app

--- resources/views/front-end/layout/footer.blade.php
+++ resources/views/front-end/layout/footer.blade.php
@@ -13,12 +13,14 @@
         <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0 contact-link">
           <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
           <div><a href="tel:{{$article->phone ?? $configration->phone}}">{{$article->phone ?? $configration->phone}}</a>
-            {{-- <br><span style="font-size: 80%">هاتف  نقل العفش</span></div> --}}
+            <br><span style="font-size: 80%">هاتف  نقل العفش</span>
+          </div>
         </div>
         <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0 contact-link">
           <i class="fab fa-whatsapp fa-3x mb-3 text-muted"></i>
           <div><a href="tel:{{$article->phone2 ?? $configration->whatsapp}}">{{$article->phone2 ?? $configration->whatsapp}}</a>
-            {{-- <br><span style="font-size: 80%">هاتف الواتس</span></div> --}}
+            <br><span style="font-size: 80%">هاتف الواتس</span>
+          </div>
         </div>
         <div class="col-lg-4 mr-auto text-center contact-link">
           <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
